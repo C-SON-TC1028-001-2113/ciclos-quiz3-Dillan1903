047291bf201e14047c77cd5ea3c9e92398e04c60
Dillan1903 <A01252454@itesm.mx>
2021-09-03 16:52:13 +0000

    05 Promedio sencillo

--- assignments/05PromedioSencillo/src/exercise.py
+++ assignments/05PromedioSencillo/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+    n = int(input())
+    j = 0
+    p = 0.0
+    while j<n:
+        e = float(input())
+        p = p+e
+        j = j+1
+    r = (p/n)
+    print(r)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
